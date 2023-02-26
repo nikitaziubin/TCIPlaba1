63ea30ff02745372815b09803b78bdf3da6a8052
nikita.ziubin <nikita.ziubin@gmail.com>
2023-02-26 18:10:57 +0200

    genetate controlers for matches participantes and stadiums for prototyping

--- TCIPlaba1/Models/Match.cs
+++ TCIPlaba1/Models/Match.cs
@@ -1,13 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace TCIPlaba1;
 
 public partial class Match
 {
     public int Id { get; set; }
-
+    [Display(Name = "Дата")]
     public DateTime Date { get; set; }
-
+    [Display(Name = "Дівізіон")]
     public byte Division { get; set; }
 }
--- TCIPlaba1/Models/Participant.cs
+++ TCIPlaba1/Models/Participant.cs
@@ -1,17 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace TCIPlaba1;
 
 public partial class Participant
 {
     public int Id { get; set; }
-
+    [Display(Name = "Матч")]
     public int Match { get; set; }
-
+    [Display(Name = "Команда")]
     public int Team { get; set; }
-
+    [Display(Name = "Роль Команди")]
     public byte TeamRole { get; set; }
-
+    [Display(Name = "Голи")]
     public byte Goals { get; set; }
 }
